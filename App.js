5a51583a2b3b4b82b776ee9cf6066851897e8e12
Daniel Caro <94130991+CaroDaniel@users.noreply.github.com>
2025-04-06 13:05:31 +0000

    main

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import NumberGame from "./NumberGame";
 
 export default function App() {
-  <NumberGame />;
+  return <NumberGame />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
